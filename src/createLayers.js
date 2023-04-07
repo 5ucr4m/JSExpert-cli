970425b6e29a34523c1bae9b69c732ae55e3cc94
Marcus Vinicius <5ucr4m@gmail.com>
2023-04-07 13:24:46 -0300

    aula 02

---
+++ src/createLayers.js
@@ -0,0 +1,12 @@
+import fsPromises from 'fs/promises'
+import fs from 'fs'
+
+export async function createLayersIfNotExists ({ mainPath, defaultMainFolder, layers  }) {
+
+    const defaultPath = `${mainPath}/${defaultMainFolder}`;
+    const folderToCreate = layers.filter(layer => !fs.existsSync(layer));
+    const result = folderToCreate .map(folder => fsPromises.mkdir(`${defaultPath}/${folder}`, { recursive : true }));
+
+    return Promise.all(result);
+
+}
